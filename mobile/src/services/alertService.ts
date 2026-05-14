34090f240d9d445d654080f8c09f877a57b91515
Mohamad Mzafar <mohamad.mzafar@georgebrown.ca>
2026-05-14 00:12:17 -0400

    Added more audio files

--- mobile/src/services/alertService.ts
+++ mobile/src/services/alertService.ts
@@ -14,6 +14,18 @@ const alarmDescriptionSoundMap = {
   FIRE: require('../../assets/AlarmDescriptions/007.m4a'),
   DOOR_OPEN: require('../../assets/AlarmDescriptions/060.m4a'),
   CPU_OVERLOAD: require('../../assets/AlarmDescriptions/072.m4a'),
+  COMM_LOSS: require('../../assets/AlarmDescriptions/030.m4a'),
+  DISK_FULL: require('../../assets/AlarmDescriptions/073.m4a'),
+  EMERGENCY_STOP: require('../../assets/AlarmDescriptions/062.m4a'),
+  FAN_FAILURE: require('../../assets/AlarmDescriptions/075.m4a'),
+  GAS_LEAK: require('../../assets/AlarmDescriptions/009.m4a'),
+  GENERATOR_FAULT: require('../../assets/AlarmDescriptions/024.m4a'),
+  HIGH_PRESSURE: require('../../assets/AlarmDescriptions/041.m4a'),
+  HIGH_TEMPERATURE: require('../../assets/AlarmDescriptions/034.m4a'),
+  INTRUSION: require('../../assets/AlarmDescriptions/061.m4a'),
+  LOW_BATTERY: require('../../assets/AlarmDescriptions/023.m4a'),
+  LOW_PRESSURE: require('../../assets/AlarmDescriptions/042.m4a'),
+  LOW_TEMPERATURE: require('../../assets/AlarmDescriptions/035.m4a'),
 } as const
 
 let player: ReturnType<typeof createAudioPlayer> | null = null
