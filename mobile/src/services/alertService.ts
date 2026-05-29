6fd11ef2225f43ba6194edaab3fc1d2b8144d735
Mohamad Mzafar <mohamad.mzafar@georgebrown.ca>
2026-05-29 01:03:52 -0400

    converted the audio files to mp3 format

--- mobile/src/services/alertService.ts
+++ mobile/src/services/alertService.ts
@@ -3,29 +3,25 @@ import { createAudioPlayer, setAudioModeAsync } from 'expo-audio'
 import type { Alarm } from '../types/alarm'
 
 const severitySoundMap = {
-  Informational: require('../../assets/Informational.m4a'),
-  Problem: require('../../assets/Problem.m4a'),
-  Warning: require('../../assets/Warning.m4a'),
-  Critical: require('../../assets/Critical.m4a'),
-  Panic: require('../../assets/Panic.m4a'),
+  Informational: require('../../assets/Informational.mp3'),
+  Problem: require('../../assets/Problem.mp3'),
+  Warning: require('../../assets/Warning.mp3'),
+  Critical: require('../../assets/Critical.mp3'),
+  Panic: require('../../assets/Panic.mp3'),
 } as const
 
 const alarmDescriptionSoundMap = {
-  FIRE: require('../../assets/AlarmDescriptions/007.m4a'),
-  DOOR_OPEN: require('../../assets/AlarmDescriptions/060.m4a'),
-  CPU_OVERLOAD: require('../../assets/AlarmDescriptions/072.m4a'),
-  COMM_LOSS: require('../../assets/AlarmDescriptions/030.m4a'),
-  DISK_FULL: require('../../assets/AlarmDescriptions/073.m4a'),
-  EMERGENCY_STOP: require('../../assets/AlarmDescriptions/062.m4a'),
-  FAN_FAILURE: require('../../assets/AlarmDescriptions/075.m4a'),
-  GAS_LEAK: require('../../assets/AlarmDescriptions/009.m4a'),
-  GENERATOR_FAULT: require('../../assets/AlarmDescriptions/024.m4a'),
-  HIGH_PRESSURE: require('../../assets/AlarmDescriptions/041.m4a'),
-  HIGH_TEMPERATURE: require('../../assets/AlarmDescriptions/034.m4a'),
-  INTRUSION: require('../../assets/AlarmDescriptions/061.m4a'),
-  LOW_BATTERY: require('../../assets/AlarmDescriptions/023.m4a'),
-  LOW_PRESSURE: require('../../assets/AlarmDescriptions/042.m4a'),
-  LOW_TEMPERATURE: require('../../assets/AlarmDescriptions/035.m4a'),
+  FIRE: require('../../assets/AlarmDescriptions/007.mp3'),
+  GAS_LEAK: require('../../assets/AlarmDescriptions/009.mp3'),
+  LOW_BATTERY: require('../../assets/AlarmDescriptions/023.mp3'),
+  GENERATOR_FAULT: require('../../assets/AlarmDescriptions/024.mp3'),
+  COMM_LOSS: require('../../assets/AlarmDescriptions/030.mp3'),
+  HIGH_TEMPERATURE: require('../../assets/AlarmDescriptions/034.mp3'),
+  LOW_TEMPERATURE: require('../../assets/AlarmDescriptions/035.mp3'),
+  HIGH_PRESSURE: require('../../assets/AlarmDescriptions/041.mp3'),
+  LOW_PRESSURE: require('../../assets/AlarmDescriptions/042.mp3'),
+  OVERCURERENT: require('../../assets/AlarmDescriptions/050.mp3'),
+  
 } as const
 
 let player: ReturnType<typeof createAudioPlayer> | null = null
