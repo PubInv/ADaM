32330245811ae99335304b93dcab7e417a23fb0c
Mohamad Mzafar <mohamad.mzafar@georgebrown.ca>
2026-08-15 18:16:33 -0400

    Updated the license section to AGPL

--- ADaMpy/subscribe.py
+++ ADaMpy/subscribe.py
@@ -1,19 +1,19 @@
 # subscribe.py file is a simple MQTT client that subscribes to a topic for receiving alarms in GPAP format, 
 # maintains an internal list of active alarms, and allows the user to acknowledge them via command-line input.
 #
-# Copyright (C) 2026  Mohamad Mzafar.
+# Copyright (C) 2026  Saicharan Vishwanatha, Mohamad Mzafar.
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import json
